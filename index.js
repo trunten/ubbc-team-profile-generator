b91939f562121bf08556cacb9137151b83438505
Jonathan Davis <trunten@btinternet.com>
2023-03-05 21:20:30 +0000

    Finished basic output. All tests pass

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const { resolve, join } = require("path");
 const fs = require("fs");
+const render = require("./src/page-template.js");
 
 const OUTPUT_DIR = resolve(__dirname, "output");
 const outputPath = join(OUTPUT_DIR, "team.html");
-
-const render = require("./src/page-template.js");
-
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
 const team = [];
 
 function start () {
